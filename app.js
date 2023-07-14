c61dd2bcfa8da3973ddb6dc2e6760cac806b3319
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-14 17:30:46 +0300

    done with blocking codes example

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request event");
-  res.end("Hello World");
+  // home
+  if (req.url === "/") {
+    res.end("home page");
+    return;
+  }
+  // about
+  if (req.url === "/about") {
+    //blocking code
+    for (let i = 0; i < 200; i++) {
+      for (let j = 0; j < 1000; j++) {
+        log(`${i} and ${j}`);
+      }
+    }
+    res.end("about page");
+    return;
+  }
+  //error
+  res.end("error page");
 });
 
 server.listen(5000, () => {
-  console.log("Server listening on port : 5000....");
+  log("listening to port 500");
 });
