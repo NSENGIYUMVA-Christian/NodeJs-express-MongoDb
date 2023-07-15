0eadfd4e54c379e44ca711c480e77001055dc8e5
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-15 20:07:33 +0300

    Streams useful use case

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const { log } = require("console");
-const { createReadStream } = require("fs");
+var http = require("http");
+var fs = require("fs");
 
-const stream = createReadStream("./content/big.txt", {
-  highWaterMark: 90000,
-  encoding: "utf8",
-});
-
-stream.on("data", (result) => {
-  log(result);
-});
-
-stream.on("data", (error) => {
-  log(error);
-});
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream("./content/big.txt", "utf8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5000);
