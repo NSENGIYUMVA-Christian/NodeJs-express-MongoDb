8556bbe6642ad9d1515084501a439b1d67c5f367
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-15 19:17:49 +0300

    Event emitter basic setup

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
 const { log } = require("console");
-const { readFile, writeFile } = require("fs").promises;
-const util = require("util");
+const EventEmitter = require("events");
+const customEmitter = new EventEmitter();
 
-// const readFilePromise = util.promisify(readFile);
-// const WriteFilePromise = util.promisify(writeFile);
+customEmitter.on("response", () => {
+  log(`Data received`);
+});
 
-const start = async () => {
-  try {
-    // const first = await readFilePromise("./content/first.txt", "utf8");
-    // const second = await readFilePromise("./content/second.txt", "utf8");
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile("./content/result-utilPromise.txt", "it is just good", {
-      flag: "a",
-    });
-    log(first);
-    log(second);
-  } catch (error) {
-    log(error);
-  }
-};
-
-start();
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       // handle error
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-
-// getText("./content/first.txt")
-//   .then((res) => log(res))
-//   .catch((err) => log(err));
+customEmitter.emit("response");
