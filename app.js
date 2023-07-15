3c891afb1acc74e2e3e263046e410c677ff9bd26
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-15 19:59:25 +0300

    reading big file

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const { log } = require("console");
 const { createReadStream } = require("fs");
 
-const stream = createReadStream("./content/big.txt");
+const stream = createReadStream("./content/big.txt", {
+  highWaterMark: 90000,
+  encoding: "utf8",
+});
 
 stream.on("data", (result) => {
   log(result);
+});
+
+stream.on("data", (error) => {
+  log(error);
 });
