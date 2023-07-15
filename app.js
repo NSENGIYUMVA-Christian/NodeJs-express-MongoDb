c50ab631aff95d65683658be0191745fe32fe639
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-15 19:37:48 +0300

    Handling requests with events emitter

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const { log } = require("console");
-const EventEmitter = require("events");
-const customEmitter = new EventEmitter();
+const http = require("http");
 
-customEmitter.on("response", (name, id) => {
-  log(`Data received ${name} with id of ${id}`);
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer();
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome");
 });
 
-customEmitter.on("response", () => {
-  log(`Some other logic here`);
-});
-
-customEmitter.emit("response", "Chris", "0");
+server.listen(5000);
