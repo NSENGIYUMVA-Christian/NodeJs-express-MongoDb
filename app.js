d80ba847ac4ba022a5ec3a8404fd3a5816342adc
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-15 18:56:59 +0300

    Cover async await approach

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const { log } = require("console");
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+const util = require("util");
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      // handle error
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-// getText("./content/first.txt")
-//   .then((res) => log(res))
-//   .catch((err) => log(err));
+// const readFilePromise = util.promisify(readFile);
+// const WriteFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
-
+    // const first = await readFilePromise("./content/first.txt", "utf8");
+    // const second = await readFilePromise("./content/second.txt", "utf8");
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile("./content/result-utilPromise.txt", "it is just good", {
+      flag: "a",
+    });
     log(first);
     log(second);
   } catch (error) {
@@ -31,3 +22,20 @@ const start = async () => {
 };
 
 start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       // handle error
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((res) => log(res))
+//   .catch((err) => log(err));
