643f743e7f6510b86e7760f08dc00b48a6891ecf
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-18 17:39:45 +0300

    covered Http header,body, Mime types , status code

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,15 @@
-console.log('Express Tutorial')
+const { log } = require("console");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // providing meta data about our response
+  res.writeHead(200, { "content-type": "text/html" });
+  // body
+  res.write("<h1>Home page bro</p>");
+  // explicity end request
+  res.end();
+});
+
+server.listen(5000, () => {
+  log("server is running on port 5000");
+});
