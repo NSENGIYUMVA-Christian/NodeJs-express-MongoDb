ca2609ea3eace680438762057d346b80f78a1df3
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-18 17:58:25 +0300

    Basic server with headers and body to handle pages req

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,12 +2,26 @@ const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  // providing meta data about our response
-  res.writeHead(200, { "content-type": "text/html" });
-  // body
-  res.write("<h1>Home page bro</p>");
-  // explicity end request
-  res.end();
+  const url = req.url;
+
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home page bro</p>");
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About page</p>");
+    res.end();
+  }
+  // 404 resource not found
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<p>404 resource not found</p> <a href='/'>Back home</a>");
+    res.end();
+  }
 });
 
 server.listen(5000, () => {
