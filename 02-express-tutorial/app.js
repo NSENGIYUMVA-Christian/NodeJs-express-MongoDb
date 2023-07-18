5c458e6c4b5280ffc1d2bc724f661ddbc5e8668d
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-18 19:03:29 +0300

    Sending html,css,svgs and js in one req

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,15 +3,38 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 //get all files
-const home = readFileSync("./index.html", "utf8");
+const home = readFileSync("./navbar-app/index.html", "utf8");
+const style = readFileSync("./navbar-app/styles.css", "utf8");
+const logo = readFileSync("./navbar-app/logo.svg", "utf8");
+const browserApp = readFileSync("./navbar-app/browser-app.js", "utf8");
 
+// setting server
 const server = http.createServer((req, res) => {
+  log(req.url);
   const url = req.url;
 
   // home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(home);
+    res.end();
+  }
+  //styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(style);
+    res.end();
+  }
+  //javascript file
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "application/javascript" });
+    res.write(browserApp);
+    res.end();
+  }
+  //logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(logo);
     res.end();
   }
   //about page
