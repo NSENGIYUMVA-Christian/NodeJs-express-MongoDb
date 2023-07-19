98c62e1cf67163dd496aec4680c43c968f796be8
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-19 12:49:23 +0300

    Sending filtered products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,16 @@ const { products } = require("./data");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home page</h1> <a href="/api/products">Products<a/>');
+});
+//sending products as json
+app.get("/api/products", (req, res) => {
+  // filtering data to send
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
 
 app.listen(5000, () => {
