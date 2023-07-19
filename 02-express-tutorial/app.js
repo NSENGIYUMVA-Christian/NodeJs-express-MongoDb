391f612294ba9f1d9c24600fe8dcf0ed20d975f0
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-19 18:04:48 +0300

    query string params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -27,10 +27,34 @@ app.get("/api/products/:productId", (req, res) => {
   }
   res.json(singleProduct);
 });
+
 //Sending product based on specified reviews
 app.get("/api/products/:productId/reviews/:reviewId", (req, res) => {
   // filtering data to send
   res.send(`Here is the review with id of ${req.params.reviewId}`);
+});
+
+//query string params. filtered url
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  // check if search is truthy value
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  // check if limit is truthy value
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  // if the sorted products returns empty array through query string params
+  if (sortedProducts.length === 0) {
+    // res.status(200).send(`No product matched your search`);
+    res.status(200).json({ success: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
