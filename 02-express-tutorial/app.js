8dbfaa0115386ecb03b60d57ecebea5d88f1eb34
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-19 11:43:59 +0300

    intro to app.use(express.static),middleware and static assets

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,19 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-//app.get
-// home page
+app.use(express.static("./public"));
+
+// home
 app.get("/", (req, res) => {
-  res.status(200).send("Home page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
-// about page
-app.get("/about", (req, res) => {
-  res.status(200).send("about page");
+
+// error
+app.get("*", (req, res) => {
+  res.status(404).send("Resource not found");
 });
-// handles 404, like when user hits invalid url
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found<h1>");
-});
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
 
 app.listen(5000, () => {
   console.log("Listening to port 5000");
