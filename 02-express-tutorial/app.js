2884123b07df62f10b1103ae5270e38cbb043e6a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-20 10:51:52 +0300

    Handling request with js form

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,14 +6,29 @@ const app = express();
 // our static asset---- launching our frontend app
 app.use(express.static("./methods-public"));
 
-//getting and parsing form data
+//getting form data from raw html
 app.use(express.urlencoded({ extended: false }));
+
+// parsing incoming json data from js frontend
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   return res.status(200).json({ success: true, data: people });
 });
 
-// post method
+// post method on js frontend request
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+
+  return res.status(201).json({ success: true, person: name });
+});
+
+// post method on traditional html forms
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (!name) {
