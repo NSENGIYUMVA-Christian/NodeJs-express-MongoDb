eabbc906c771f2e2f650c288f2a5ef0a115be68a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-20 18:58:15 +0300

    Basic structure for all routes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasks = require("./routes/tasks");
+const app = express();
+
+//middleware to parse incoming json
+app.use(express.json());
+
+//routes
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`listening to port ${port}`));
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,18 @@
+const getAllTasks = (req, res) => {
+  res.send("All items");
+};
+
+const createTask = (req, res) => {
+  res.send("Create task");
+};
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
