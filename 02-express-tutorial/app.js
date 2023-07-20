d90e4ebc40083588b103a11a6e6ec0bbe06a035f
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-20 11:59:17 +0300

    Updating data with put http verb

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -28,12 +28,46 @@ app.post("/api/people", (req, res) => {
   return res.status(201).json({ success: true, person: name });
 });
 
+// extra example to practice postman
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, data: "Please provide user name" });
+  } else {
+    return res.status(200).json({ success: true, data: [...people, name] });
+  }
+});
+
 // post method on traditional html forms
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res.status(401).send("Please provide credentials");
   } else return res.status(200).send(`welcome ${name}`);
+});
+
+// put method (edit or updating data)
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((eachPerson) => eachPerson.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ message: false, msg: `No person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
