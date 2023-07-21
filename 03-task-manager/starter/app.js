bd6717d8920d8329573e80060795ae51a4ae4b45
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-21 11:52:41 +0300

    Models and schema

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,8 @@
-require("./db/connect");
 const express = require("express");
 const tasks = require("./routes/tasks");
+const connectDb = require("./db/connect");
+require("dotenv").config();
+
 const app = express();
 
 //middleware to parse incoming json
@@ -11,4 +13,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`listening to port ${port}`));
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URL);
+    app.listen(port, console.log(`server is listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
