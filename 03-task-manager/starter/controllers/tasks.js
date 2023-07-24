a9785706d481b6f66372bc4a9aa514890cba964d
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-24 16:22:55 +0300

    Alternatives approacs of sending response

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,6 +5,9 @@ const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
+    // alternative response
+    // res.status(200).json({ tasks,amount:tasks.length });
+    // res.status(200).json({success:true, data:{tasks} });
   } catch (error) {
     res.status(500).json({ msg: "There was an internal server error" });
   }
