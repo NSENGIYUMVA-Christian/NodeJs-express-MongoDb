dac617d5582bb5eade5d74fd104fb3cb34be209e
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-24 13:17:13 +0300

    Handling errors in validators

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,8 +5,12 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "There was an internal server error" });
+  }
 };
 const getTask = (req, res) => {
   res.send("get single task");
