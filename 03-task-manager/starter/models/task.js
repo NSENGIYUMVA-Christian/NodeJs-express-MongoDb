4a2d8a9636ff107ec562c50a02d6777a3c4ef33b
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-24 12:59:28 +0300

    basic validation in mangoose

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  //basic validation to avoid restricted behaviors for instance in our project, adding empty values to our database
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxLength: [20, "Name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
