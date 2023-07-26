a6fe66907f7b5a37194a2c5a604d47cd4c233f14
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-26 18:20:10 +0300

    pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,7 @@
 const product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({}).select("name price");
+  const products = await product.find({}).select("name price").limit(4);
   res.status(200).json({ products, amountOfProducts: products.length });
 };
 
@@ -30,11 +30,20 @@ const getAllProducts = async (req, res) => {
     result = result.sort("createdAt");
   }
 
-  // select
+  // select particular fields or properties
   if (fields) {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  // limit and skip
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  // 23 =
 
   // returned products
   const products = await result;
