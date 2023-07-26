1f6f896092cbc8679358d45e0f7222fbffccaa30
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-26 18:48:32 +0300

    filtering by numericValue, regexes and methods(,,..)

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,13 @@
 const product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({}).select("name price").limit(4);
+  const products = await product.find({ price: { $lt: 30 } });
   res.status(200).json({ products, amountOfProducts: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   //console.log(req.query);
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -43,7 +43,22 @@ const getAllProducts = async (req, res) => {
 
   result = result.skip(skip).limit(limit);
 
-  // 23 =
+  //numericFilters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `_${operatorMap[match]}_`
+    );
+    console.log(filters);
+  }
 
   // returned products
   const products = await result;
