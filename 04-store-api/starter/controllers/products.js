68b8bfe6582139cfa44a112afccc043af1c104f4
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-26 16:37:40 +0300

    regex operators in queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   //console.log(req.query);
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -16,7 +16,11 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
   console.log(queryObject);
+
   const products = await product.find(queryObject);
   res.status(200).json({ products, amountOfProducts: products.length });
 };
