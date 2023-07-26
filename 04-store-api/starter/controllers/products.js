511696bcad15614a40efa62ef2d8940584040706
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-26 17:27:59 +0300

    a bit complex sorting

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   //console.log(req.query);
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -19,9 +19,16 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
 
-  const products = await product.find(queryObject);
+  let result = product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+
   res.status(200).json({ products, amountOfProducts: products.length });
 };
 
