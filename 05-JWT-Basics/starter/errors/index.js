61344f7bbca7175a2dda7a5880b638b3d8ec8edd
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-29 11:18:20 +0300

    added new BadRequest and Unauth error classes and refactoring all error classes

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,5 @@
+const UnauthenticatedError = require("./unauthenticated");
+const CustomAPIError = require("./unauthenticated");
+const BadRequest = require("./bad-request");
+
+module.exports = { UnauthenticatedError, CustomAPIError, BadRequest };
