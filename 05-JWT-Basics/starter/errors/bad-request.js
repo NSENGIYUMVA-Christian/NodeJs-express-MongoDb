9ea9110626c2de48f09bd41756ca1d077fa040c7
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-29 11:26:07 +0300

    using status codes package to handle status codes smoothly

--- 05-JWT-Basics/starter/errors/bad-request.js
+++ 05-JWT-Basics/starter/errors/bad-request.js
@@ -1,9 +1,10 @@
+const { StatusCode } = require("http-status-codes");
 const CustomAPIError = require("./custom-error");
 
 class BadRequest extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 400;
+    this.statusCode = StatusCode.BadRequest;
   }
 }
 
--- 05-JWT-Basics/starter/errors/unauthenticated.js
+++ 05-JWT-Basics/starter/errors/unauthenticated.js
@@ -1,9 +1,10 @@
+const { StatusCode } = require("http-status-codes");
 const CustomAPIError = require("./custom-error");
 
 class UnauthenticatedError extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 401;
+    this.statusCode = StatusCode.UNAUTHORIZED;
   }
 }
 
