4aef4d4f0030f65070d15290a350a67b54dd2676
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-29 10:04:19 +0300

    Sending back custom error if authorization header not provided

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -26,6 +26,16 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  //console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided ", 401);
+  }
+  // get token
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello john doe`,
