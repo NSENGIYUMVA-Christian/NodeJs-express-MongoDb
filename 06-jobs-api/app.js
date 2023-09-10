c1048f8ee600ef8c02dea97201058d791c3c0949
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-10 18:55:52 +0300

    connected to DB  successfully

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 //connect db
-
+const connectDB = require("./db/connect");
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -27,6 +27,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
