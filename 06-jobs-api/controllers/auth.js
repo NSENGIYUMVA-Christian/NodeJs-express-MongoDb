69a92ba5c0097997d82378d0bd1f2928ff29ce3a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-10 19:49:21 +0300

    handling empty req.body with moongose validators

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 const register = async (req, res) => {
   //create a new user
   const user = await User.create({ ...req.body });
