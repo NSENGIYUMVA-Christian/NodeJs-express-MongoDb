7cf86a9c7fb8f99fd46a7ddaa2741b34f0735944
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-10 19:09:02 +0300

    added user model

---
+++ 06-jobs-api/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide a name"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide a email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a password"],
+    minLength: 6,
+    maxLength: 12,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
