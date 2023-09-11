77903db29f0f07edf04eb57aa7fc9a1dc4dbb358
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-11 09:31:53 +0300

    basic login verification

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+// register controller
 const register = async (req, res) => {
   //create a new user
   const user = await User.create({ ...req.body });
@@ -8,8 +9,24 @@ const register = async (req, res) => {
   // send token
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
+// login controller
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  //verifying if login fields are provided
+  if (!email || !password) {
+    throw new BadRequestError("PLease provide email and password");
+  }
+  // get user from db
+  const user = await User.findOne({ email });
+  // if a user doesn't exist in our db throw unauthanticated error
+  //compare password
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  // if a user exists
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
