b8ca9a9f1131c270ca827a294078ff0df689ca5b
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-11 20:35:35 +0300

    get single job successfully

--- 06-jobs-api/controllers/jobs.js
+++ 06-jobs-api/controllers/jobs.js
@@ -10,7 +10,17 @@ const getAllJobs = async (req, res) => {
 
 /////// get a single job
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  // if wrong id
+  if (!job) {
+    throw new NotFoundError(`No  job with id ${jobId}`);
+  }
+  // if correct
+  res.status(StatusCodes.OK).json({ job });
 };
 
 //// create a job
