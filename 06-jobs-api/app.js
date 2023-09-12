d80e00ad82f82296701bc68f71414bbf0beb5efd
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-12 09:59:13 +0300

    added security packages including'helmet,cors,xss and rate limit

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+// extra security package
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -7,6 +13,7 @@ const app = express();
 const connectDB = require("./db/connect");
 //import auth middleware
 const authenticateUser = require("./middleware/authentication");
+
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -16,7 +23,18 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
-// extra packages
+// security packages
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
--- 06-jobs-api/middleware/error-handler.js
+++ 06-jobs-api/middleware/error-handler.js
@@ -26,6 +26,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, PLease choose another value`;
     customError.statusCode = 400;
   }
+  // cast error
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id:${err.value}`;
+    customError.statusCode = 404;
+  }
+
   //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
