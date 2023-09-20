45dabe59bf98393c095e6e168735f2a248121f7f
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-20 11:34:18 +0300

    done with stripe payment project

--- 09-stripe-payment/app.js
+++ 09-stripe-payment/app.js
@@ -1,19 +1,20 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // controller
-
+const stripeController = require("./controllers/stripeController");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // stripe
+app.post("/stripe", stripeController);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
--- 09-stripe-payment/public/browser-app.js
+++ 09-stripe-payment/public/browser-app.js
@@ -1,22 +1,22 @@
 const purchase = [
-  { id: '1', name: 't-shirt', price: 1999 },
-  { id: '2', name: 'shoes', price: 4999 },
+  { id: "1", name: "t-shirt", price: 1999 },
+  { id: "2", name: "shoes", price: 4999 },
 ];
 const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  "pk_test_51N4LlsIvmedeIJBgfU1OFRVyVJxyByfZWLIm2uvHfY8MGG22uQXJ6KIXUie5kWeVC6lKAbV4YF0Ffgctcc5TiNOL009IpRaUbM"
 );
 
 // The items the customer wants to buy
 
 // Disable the button until we have Stripe set up on the page
-document.querySelector('button').disabled = true;
-fetch('/stripe', {
-  method: 'POST',
+document.querySelector("button").disabled = true;
+fetch("/stripe", {
+  method: "POST",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   body: JSON.stringify({ purchase, total_amount, shipping_fee }),
 })
@@ -28,35 +28,35 @@ fetch('/stripe', {
 
     var style = {
       base: {
-        color: '#32325d',
-        fontFamily: 'Arial, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-          color: '#32325d',
+        color: "#32325d",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
         },
       },
       invalid: {
-        fontFamily: 'Arial, sans-serif',
-        color: '#fa755a',
-        iconColor: '#fa755a',
+        fontFamily: "Arial, sans-serif",
+        color: "#fa755a",
+        iconColor: "#fa755a",
       },
     };
 
-    var card = elements.create('card', { style: style });
+    var card = elements.create("card", { style: style });
     // Stripe injects an iframe into the DOM
-    card.mount('#card-element');
+    card.mount("#card-element");
 
-    card.on('change', function (event) {
+    card.on("change", function (event) {
       // Disable the Pay button if there are no card details in the Element
-      document.querySelector('button').disabled = event.empty;
-      document.querySelector('#card-error').textContent = event.error
+      document.querySelector("button").disabled = event.empty;
+      document.querySelector("#card-error").textContent = event.error
         ? event.error.message
-        : '';
+        : "";
     });
 
-    var form = document.getElementById('payment-form');
-    form.addEventListener('submit', function (event) {
+    var form = document.getElementById("payment-form");
+    form.addEventListener("submit", function (event) {
       event.preventDefault();
       // Complete payment when the submit button is clicked
       payWithCard(stripe, card, data.clientSecret);
@@ -91,22 +91,22 @@ var payWithCard = function (stripe, card, clientSecret) {
 var orderComplete = function (paymentIntentId) {
   loading(false);
   document
-    .querySelector('.result-message a')
+    .querySelector(".result-message a")
     .setAttribute(
-      'href',
-      'https://dashboard.stripe.com/test/payments/' + paymentIntentId
+      "href",
+      "https://dashboard.stripe.com/test/payments/" + paymentIntentId
     );
-  document.querySelector('.result-message').classList.remove('hidden');
-  document.querySelector('button').disabled = true;
+  document.querySelector(".result-message").classList.remove("hidden");
+  document.querySelector("button").disabled = true;
 };
 
 // Show the customer the error from Stripe if their card fails to charge
 var showError = function (errorMsgText) {
   loading(false);
-  var errorMsg = document.querySelector('#card-error');
+  var errorMsg = document.querySelector("#card-error");
   errorMsg.textContent = errorMsgText;
   setTimeout(function () {
-    errorMsg.textContent = '';
+    errorMsg.textContent = "";
   }, 4000);
 };
 
@@ -114,12 +114,12 @@ var showError = function (errorMsgText) {
 var loading = function (isLoading) {
   if (isLoading) {
     // Disable the button and show a spinner
-    document.querySelector('button').disabled = true;
-    document.querySelector('#spinner').classList.remove('hidden');
-    document.querySelector('#button-text').classList.add('hidden');
+    document.querySelector("button").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
   } else {
-    document.querySelector('button').disabled = false;
-    document.querySelector('#spinner').classList.add('hidden');
-    document.querySelector('#button-text').classList.remove('hidden');
+    document.querySelector("button").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
   }
 };
