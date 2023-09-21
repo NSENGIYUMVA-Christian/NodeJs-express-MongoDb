528dcf763487f6ab43c5bd68d8cdae8886d31e29
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-21 09:17:28 +0300

    done setting refister user model and schema

---
+++ 10-jobs-app-two/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide valid email",
+    ],
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    maxlength: 12,
+  },
+});
+
+module.exports = mongoose.Model("User", userSchema);
