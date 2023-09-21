b5c48d0c6be18f7f36c19e001bbf1ffa2be43b7a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-21 10:58:05 +0300

    handling tokens in user model using documents instance methods

--- 10-jobs-app-two/models/User.js
+++ 10-jobs-app-two/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -30,5 +31,12 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// document functions || instance methods
+userSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtsecret", {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
