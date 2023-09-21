f8886ea498b760fac63174ed446ff0c1f686bc36
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-21 13:30:40 +0300

    login basic setup complete

--- 10-jobs-app-two/controllers/auth.js
+++ 10-jobs-app-two/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -9,7 +9,21 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  // check if email and password is provided
+  if (!email || !password) {
+    throw new BadRequestError("PLease provide email and password ");
+  }
+  // check user if exist in DB
+  const user = await User.findOne({ email });
+  // compare password
+
+  //if no user
+  if (!user) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
