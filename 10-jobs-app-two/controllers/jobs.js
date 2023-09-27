15195949b7a7dc53d730f9b68dd2ef10608cfc3d
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-27 13:11:40 +0300

    create new job to DB successfully

--- 10-jobs-app-two/controllers/jobs.js
+++ 10-jobs-app-two/controllers/jobs.js
@@ -1,3 +1,7 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("getAllJobs");
 };
@@ -5,9 +9,13 @@ const getAllJobs = async (req, res) => {
 const getJob = async (req, res) => {
   res.send("get Job");
 };
+
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update Job");
 };
