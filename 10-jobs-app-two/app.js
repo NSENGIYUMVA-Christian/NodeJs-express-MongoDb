16c76dc28122998613333591b810f2521c5ea63a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-30 10:16:21 +0300

    added security packages helmet,xss,cors and exp rate limiter

--- 10-jobs-app-two/app.js
+++ 10-jobs-app-two/app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 const app = express();
 // connect DB
 const connectDB = require("./db/connect");
@@ -17,6 +22,16 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
