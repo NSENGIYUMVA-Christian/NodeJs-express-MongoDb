8dfbd0e2ecbfb7f48c659aca95338a2139040784
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-09-30 12:04:59 +0300

    added some basic middleware on app.js

--- 12-e-commerce-api/app.js
+++ 12-e-commerce-api/app.js
@@ -1,16 +1,31 @@
 // importing dotenv
 require("dotenv").config();
 
+require("express-async-errors");
+
 //importing express
 const express = require("express");
-
-const connectDb = require("./db/connect");
-
 const app = express();
+// connect to DATABASE import
+const connectDb = require("./db/connect");
+//middleware imports
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//express middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("e-commerce -app proj");
+});
+
+//not found url middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // server port
 const port = process.env.PORT || 3000;
-
 // starting server function
 const start = async () => {
   // connecting to DB
