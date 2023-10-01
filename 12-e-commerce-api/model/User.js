49a61ff3679b6a2c2954e759a05d3aeb7be52809
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-10-01 09:14:32 +0300

    finished setting user model

---
+++ 12-e-commerce-api/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    require: [true, "Please provide email"],
+  },
+  password: {
+    type: String,
+    require: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
