26891bc765431899d211e177f86a99d45af59207
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-10-01 10:43:51 +0300

    avoiding duplicate user email

--- 12-e-commerce-api/controller/authController.js
+++ 12-e-commerce-api/controller/authController.js
@@ -1,9 +1,26 @@
+const User = require("../model/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+////////////////register user///////////
 const register = async (req, res) => {
-  res.send("register user");
+  const { email } = req.body;
+  //check if email exist
+  const emailAlreadyExist = await User.findOne({ email });
+  if (emailAlreadyExist) {
+    throw new CustomError.BadRequestError("Email already exist");
+  }
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
+
+///////////////login user///////////
 const login = async (req, res) => {
   res.send("login user");
 };
+
+///////////////logout user///////////
 const logout = async (req, res) => {
   res.send("logout user");
 };
