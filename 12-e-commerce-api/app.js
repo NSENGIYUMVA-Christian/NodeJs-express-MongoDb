8fb265441c3b43355f7e694acaaa4e0a160ee410
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-10-04 13:26:09 +0300

    done with signed and securing cookies

--- 12-e-commerce-api/app.js
+++ 12-e-commerce-api/app.js
@@ -21,16 +21,15 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 //pre middlewares
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 // main routers
 app.get("/", (req, res) => {
-  console.log(req.cookies);
   res.send("e-commerce -app proj");
 });
 //test route
 app.get("/api/v1", (req, res) => {
-  console.log(req.cookies);
+  console.log(req.signedCookies);
   res.send("console cookies");
 });
 app.use("/api/v1/auth", authRouter);
