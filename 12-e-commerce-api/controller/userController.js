60d49c9933e19c85c0a3231d324d0c1dc343bd4a
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-10-05 17:39:39 +0300

    done with getting single user and all users

--- 12-e-commerce-api/controller/userController.js
+++ 12-e-commerce-api/controller/userController.js
@@ -1,9 +1,25 @@
+const User = require("../model/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+/////////////////get all users
 const getAllUsers = async (req, res) => {
-  res.send("get all users");
+  //get user from db
+  const users = await User.find({ role: "user" }).select("-password");
+
+  res.status(StatusCodes.OK).json({ amount: users.length, users });
 };
+
+////////////get single user
 const getSingleUser = async (req, res) => {
-  res.send("get single user");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
+
+////////////////// show current user
 const showCurrentUser = async (req, res) => {
   res.send("show current user");
 };
