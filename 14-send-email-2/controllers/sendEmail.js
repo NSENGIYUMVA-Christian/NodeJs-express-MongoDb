22233eb2622e01a86cbaba6aae146ef26b0b9943
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-12-11 13:27:31 +0200

    f:sending email wih ethireal successfully

--- 14-send-email-2/controllers/sendEmail.js
+++ 14-send-email-2/controllers/sendEmail.js
@@ -1,5 +1,26 @@
+const nodemailer = require("nodemailer");
+
 const sendEmail = async (req, res) => {
-  res.send("send email func");
+  // creating test account
+  let testAccount = await nodemailer.createTestAccount();
+  //// create transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "mertie33@ethereal.email",
+      pass: "B3kQuwUBWf8hQA9JMK",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"chris lab" <cristiannsengi@gmail.com>',
+    to: "bar@example.com",
+    subject: "hello there",
+    html: "<h2>we are sending email with nodejs</h2>",
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
